dd57cd266357a2cbda55af6c54a813e3166c2aec
Daniel Gherghetta <gherghetta@gmail.com>
2024-10-14 16:00:15 +0200

    removed unused class

--- Models/Return.cs
+++
@@ -1,9 +0,0 @@
-// namespace VideoHallen.Models;
-
-// public class Return  : Entity
-// {
-//     public DateTime TimeStamp { get; set; }
-//     public int RentalId { get; set; }
-//     public Rental Rental { get; set; } = null!;
-//     public List<Copy> ReturnedCopies { get; set; } = null!;
-// }
